bfc4113d98ae1c4c762ce30dd31851a4a86306e4
Juan Bonfante <juan.bonfante@gmail.com>
2014-04-08 17:56:54 -0500

    Fixed compatibility with Python3K in forms
    Fixed forms compatability with python 3K and django 1.6

--- zebra/forms.py
+++ zebra/forms.py
@@ -13,7 +13,7 @@ class MonospaceForm(forms.Form):
 
 class CardForm(MonospaceForm):
     last_4_digits = forms.CharField(required=True, min_length=4, max_length=4,
-        widget=forms.HiddenInput())
+                                    widget=forms.HiddenInput())
     stripe_token = forms.CharField(required=True, widget=forms.HiddenInput())
 
 
@@ -22,15 +22,14 @@ class StripePaymentForm(CardForm):
         super(StripePaymentForm, self).__init__(*args, **kwargs)
         self.fields['card_cvv'].label = "Card CVC"
         self.fields['card_cvv'].help_text = "Card Verification Code; see rear of card."
-        months = [ (m[0], u'%02d - %s' % (m[0], unicode(m[1])))
-                    for m in sorted(MONTHS.iteritems()) ]
+        months = [ (m, u'{:02} - {}'.format(m, MONTHS[m])) for m in sorted(MONTHS)]
         self.fields['card_expiry_month'].choices = months
 
     card_number = forms.CharField(required=False, max_length=20,
-        widget=NoNameTextInput())
+                                  widget=NoNameTextInput())
     card_cvv = forms.CharField(required=False, max_length=4,
-        widget=NoNameTextInput())
+                               widget=NoNameTextInput())
     card_expiry_month = forms.ChoiceField(required=False, widget=NoNameSelect(),
-        choices=MONTHS.iteritems())
+                                          choices=MONTHS)
     card_expiry_year = forms.ChoiceField(required=False, widget=NoNameSelect(),
-        choices=options.ZEBRA_CARD_YEARS_CHOICES)
+                                         choices=options.ZEBRA_CARD_YEARS_CHOICES)
